868ea3c84a9ca0db6c0088b068049a4d1e696843
SeongminChang <seongmin.chang@mail.utoronto.ca>
2022-10-09 01:30:26 -0400

    Finished Quiz 1.

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,7 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -99,7 +100,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Current count: " + i);
+        }
 
     }
 
@@ -141,9 +144,10 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
-
-        // Fill in the rest of the body here
-
+        String[] arrOfStr = to_split.split(" ");
+        for (String a: arrOfStr) {
+            ret.append(a.charAt(0));
+        }
         return ret.toString();
     }
 
@@ -170,6 +174,10 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+
+        for (int i = 1; i < arr.length; i+=2) {
+            current_sum += arr[i];
+        }
 
         return current_sum;
     }
